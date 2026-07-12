41008f317911d077204c091b329cf51e15634d7e
srilekha-cmyk <bsrilekha75@gmail.com>
2026-07-12 16:27:52 +0530

    Create programming20.py

---
+++ programming20.py
@@ -0,0 +1,10 @@
+price = float(input("Enter Price: "))
+
+discount = price * 0.10
+final_price = price - discount
+
+print("Final Price =", final_price)
+
+# Output:
+# Enter Price: 1000
+# Final Price = 900.0
