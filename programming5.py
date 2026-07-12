460a4dd69fe58b634f2c90ea40d037ec1734e881
srilekha-cmyk <bsrilekha75@gmail.com>
2026-07-12 16:01:57 +0530

    Create programming5.py

---
+++ programming5.py
@@ -0,0 +1,15 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+a, b = b, a
+
+print("After Swapping:")
+print("a =", a)
+print("b =", b)
+
+# Output:
+# Enter first number: 10
+# Enter second number: 20
+# After Swapping:
+# a = 20
+# b = 10
