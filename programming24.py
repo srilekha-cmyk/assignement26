1b7562bcd51fe824767dc16ed8c08c1498d52770
srilekha-cmyk <bsrilekha75@gmail.com>
2026-07-12 16:50:03 +0530

    Create programming24.py

---
+++ programming24.py
@@ -0,0 +1,13 @@
+n = int(input("Enter number of terms: "))
+a = 0
+b = 1
+
+for i in range(n):
+    print(a, end=" ")
+    c = a + b
+    a = b
+    b = c
+
+# Output:
+# Enter number of terms: 7
+# 0 1 1 2 3 5 8
