9d182b269d7716688c69456e51394f82f7381f1f
srilekha-cmyk <bsrilekha75@gmail.com>
2026-07-12 16:24:24 +0530

    Add simple interest calculation in programming18.py

---
+++ programming18.py
@@ -0,0 +1,13 @@
+p = int(input("Enter Principal: "))
+r = int(input("Enter Rate: "))
+t = int(input("Enter Time: "))
+
+si = (p * r * t) / 100
+
+print("Simple Interest =", si)
+
+# Output:
+# Enter Principal: 1000
+# Enter Rate: 5
+# Enter Time: 2
+# Simple Interest = 100.0
