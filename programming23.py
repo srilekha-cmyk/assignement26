336d3d92a7cc38bf23e1f47766d119c414fe4c61
srilekha-cmyk <bsrilekha75@gmail.com>
2026-07-12 16:47:50 +0530

    Add check for Automorphic Numbers in programming23.py

---
+++ programming23.py
@@ -0,0 +1,11 @@
+num = int(input("Enter a number: "))
+square = num * num
+
+if str(square).endswith(str(num)):
+    print("Automorphic Number")
+else:
+    print("Not an Automorphic Number")
+
+# Output:
+# Enter a number: 25
+# Automorphic Number
